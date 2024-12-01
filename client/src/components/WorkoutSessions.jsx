a62a9fbad3b96b32e847839086846356e93c1cc1
= <akhilrajayanikkal@gamil.com>
2024-12-01 23:32:22 +0530

    first commit

---
+++ client/src/components/WorkoutSessions.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const WorkoiutSessions = () => {
+  return (
+    <section className="workout_session">
+      <div className="wrapper">
+        <h1>TOP WORKOUT SESSION</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto,
+           dolor doloremque illo cupiditate ducimus repellat nostrum consectetur accusamus fuga.
+        </p>
+        <img src="./img5.jpg" alt="workout" />
+      </div>
+      <div className="wrapper">
+        <h1>FEATURED BOOTCAMPS</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+          Perspiciatis deserunt quasi dolorum mollitia nulla quo tempora optio iusto ex nostrum.
+        </p>
+        <div className="bootcamps">
+          <div>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Tenetur quibusdam obcaecati accusamus blanditiis numquam repellendus nulla vitae sunt quam! Aut!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Tenetur quibusdam obcaecati accusamus blanditiis numquam repellendus nulla vitae sunt quam! Aut!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Tenetur quibusdam obcaecati accusamus blanditiis numquam repellendus nulla vitae sunt quam! Aut!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Tenetur quibusdam obcaecati accusamus blanditiis numquam repellendus nulla vitae sunt quam! Aut!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default WorkoiutSessions
